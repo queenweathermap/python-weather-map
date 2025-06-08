18193447484aad78a87a02e4790eabd22049ff1f
queenworks <116941182+queenworks@users.noreply.github.com>
2025-06-08 15:53:04 +0900

    Update gpv_plotter_gsm.py

--- module/gpv_plotter_gsm.py
+++ module/gpv_plotter_gsm.py
@@ -7,7 +7,7 @@ from module.plot_300hpa_height_wind import plot_300hpa_height_wind
 from module.plot_700hpa_dindex_500hpa_temp import plot_700hpa_dindex_500hpa_temp
 from module.plot_850hpa_temp_wind_700hpa_w import plot_850hpa_temp_wind_700hpa_w
 from module.plot_850hpa_thetae_stream import plot_850hpa_thetae_stream
-from module.plot_surface_pressure_wind_precip import plot_surface_pressure_wind_precip
+from module.plot_surface_pressure_wind_precip import plot_surface_pressure_and_wind
 
 # ここにGSM用の関数だけをimportしてください
 # 必要なものが増えたら随時追加！
