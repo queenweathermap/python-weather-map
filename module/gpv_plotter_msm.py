8d95c9be28391ad46d97933fc53fc353e30c778d
queenworks <116941182+queenworks@users.noreply.github.com>
2025-06-08 11:01:31 +0900

    Update gpv_plotter_msm.py

--- module/gpv_plotter_msm.py
+++ module/gpv_plotter_msm.py
@@ -6,11 +6,11 @@
 # 必要な描画関数を一括で使えるようにします。
 # ===============================================
 
-from .plot_300hPa import plot_300hpa_height_wind_msm
-from .plot_700hpa_dindex_500hpa_temp import plot_700hpa_temp_rh_msm
-from .plot_850hpa_temp_wind_700hPa_w import plot_850hpa_temp_wind_w_msm
-from .plot_850hpa_thetae_stream import plot_850hpa_thetae_stream_msm
-from .plot_surface_pressure_wind_precip import plot_surface_pressure_and_wind_msm
+from module.plot_300hPa import plot_300hpa_height_wind_msm
+from module.plot_700hpa_dindex_500hpa_temp import plot_700hpa_temp_rh_msm
+from module.plot_850hpa_temp_wind_700hPa_w import plot_850hpa_temp_wind_w_msm
+from module.plot_850hpa_thetae_stream import plot_850hpa_thetae_stream_msm
+from module.plot_surface_pressure_wind_precip import plot_surface_pressure_and_wind_msm
 
 # MSM用の関数だけimportします
 # 関数名やimport元は、MSM用に合わせて適宜変更してください
