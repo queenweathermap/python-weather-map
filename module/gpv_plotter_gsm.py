ff5c5f1c01a95aaa052f3cce3a1a3c9da0fdf4e6
queenworks <116941182+queenworks@users.noreply.github.com>
2025-06-08 11:23:48 +0900

    Update gpv_plotter_gsm.py

--- module/gpv_plotter_gsm.py
+++ module/gpv_plotter_gsm.py
@@ -4,7 +4,7 @@
 # ===============================================
 
 from module.plot_300hpa import plot_300hpa_height_wind
-from module.plot_700hpa_dindex_500hpa_temp import plot_700hpa_temp_rh
+from module.plot_700hpa_dindex_500hpa_temp import plot_700hpa_dindex_500hpa_temp
 from module.plot_850hpa_temp_wind_700hPa_w import plot_850hpa_temp_wind_w
 from module.plot_850hpa_thetae_stream import plot_850hpa_thetae_stream
 from module.plot_surface_pressure_wind_precip import plot_surface_pressure_and_wind
