6ca4f094bb3fe663395a547c972365a4e66bcec2
queenworks <116941182+queenworks@users.noreply.github.com>
2025-06-09 15:31:04 +0900

    Update main_weather_batch.py

--- scripts/main_weather_batch.py
+++ scripts/main_weather_batch.py
@@ -5,11 +5,11 @@ from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
 # ========== 設定 ==========
-SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]  # GitHub ActionsのSecretから供給
+SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")  # GitHub ActionsのSecretから供給
 if not SLACK_BOT_TOKEN:
     print("[ERROR] SLACK_BOT_TOKENが設定されていません")
     # 必要なら sys.exit(1)
-SLACK_CHANNEL = "#your-channel"           # チャンネルIDまたは名称
+SLACK_CHANNEL = "#C08988S0SRY"           # チャンネルIDまたは名称
 WORKDIR = "/workspace"                    # Docker内絶対パス
 IMG_GSM = "gsm.png"
 IMG_MSM = "msm.png"
