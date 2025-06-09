102af7f88b0d6a016c0e451ab88d038a73ed5a23
queenworks <116941182+queenworks@users.noreply.github.com>
2025-06-09 14:08:01 +0900

    Update gpv_panel_daily_gsm.py

--- scripts/gpv_panel_daily_gsm.py
+++ scripts/gpv_panel_daily_gsm.py
@@ -26,9 +26,9 @@ from module.gpv_plotter_gsm import (
     plot_700hpa_dindex_500hpa_temp_gsm,
     plot_850hpa_temp_wind_700hpa_w_gsm,
     plot_850hpa_thetae_stream_gsm,
-    # plot_925hpa_temp_wind_dindex_gsm,
+    plot_925hpa_temp_wind_dindex_gsm,
     plot_surface_pressure_and_wind_gsm,
-    # plot_emagram_gsm,
+    plot_emagram_gsm,
 )
 
 # --- Slack送信用ユーティリティ
