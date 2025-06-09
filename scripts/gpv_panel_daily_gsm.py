a5293658c877b059ad5d86e355b7d1a41f478cc4
queenworks <116941182+queenworks@users.noreply.github.com>
2025-06-09 11:18:06 +0900

    Update gpv_panel_daily_gsm.py

--- scripts/gpv_panel_daily_gsm.py
+++ scripts/gpv_panel_daily_gsm.py
@@ -11,7 +11,7 @@ from pathlib import Path
 
 import numpy as np
 import matplotlib.pyplot as plt
-plt.rcParams['font.family'] = 'IPAexGothic'
+plt.rcParams['font.family'] = 'IPAGothic'
 import cartopy.crs as ccrs
 import pandas as pd
 import xarray as xr
