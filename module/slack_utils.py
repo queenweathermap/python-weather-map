6b2c37de35ae2a74711d58c4c5920ec66f25049b
queenworks <116941182+queenworks@users.noreply.github.com>
2025-06-09 22:32:51 +0900

    Update slack_utils.py

--- module/slack_utils.py
+++ module/slack_utils.py
@@ -2,15 +2,15 @@
 import os
 from slack_sdk import WebClient
 
-def send_file_to_slack(image_path, channel):
+def send_file_to_slack(image_path, channel, initial_comment="天気図をお届けします！"):
     bot_token = os.environ["SLACK_BOT_TOKEN"]
     client = WebClient(token=bot_token)
     try:
-        response = client.files_upload_v2(
+        response = client.files_upload(
             channels=channel,
             file=image_path,
             title=os.path.basename(image_path),
-            initial_comment="天気図をお届けします！"
+            initial_comment=initial_comment
         )
         print("[Slack送信] status:", response.status_code if hasattr(response, "status_code") else "OK", response)
     except Exception as e:
