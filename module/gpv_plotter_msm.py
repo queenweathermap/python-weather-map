398653f3b720bd309783253a2fb0712c1a4c7015
queenworks <116941182+queenworks@users.noreply.github.com>
2025-06-09 13:14:11 +0900

    Update gpv_plotter_msm.py

--- module/gpv_plotter_msm.py
+++ module/gpv_plotter_msm.py
@@ -7,13 +7,13 @@
 # ===============================================
 
 # from module.plot_300hpa_height_wind import plot_300hpa_height_wind_msm
-# from module.plot_500hpa_vorticity import plot_500hpa_vorticity_msm
+from module.plot_500hpa_vorticity import plot_500hpa_vorticity_msm
 from module.plot_700hpa_dindex_500hpa_temp import plot_700hpa_dindex_500hpa_temp_msm
 from module.plot_850hpa_temp_wind_700hpa_w import plot_850hpa_temp_wind_700hpa_w_msm
 from module.plot_850hpa_thetae_stream import plot_850hpa_thetae_stream_msm
 from module.plot_925hpa_temp_wind_dindex import plot_925hpa_temp_wind_dindex_msm
 from module.plot_surface_pressure_wind_precip import plot_surface_pressure_and_wind_msm
-from module.plot_emagram import plot_emagram_msm
+# from module.plot_emagram import plot_emagram_msm
 
 
 # MSM用の関数だけimportします
