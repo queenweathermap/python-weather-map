914086393198bed1f8546a169d2eb3a259220f60
queenworks <116941182+queenworks@users.noreply.github.com>
2025-06-10 10:19:50 +0900

    Update gpv_panel_daily_msm_akita.py

--- scripts/gpv_panel_daily_msm_akita.py
+++ scripts/gpv_panel_daily_msm_akita.py
@@ -125,15 +125,17 @@ def make_local_weather_panel(ds, times, save_path):
         dsi_point = ds.sel(
             latitude=AKITA_PIN_LAT, longitude=AKITA_PIN_LON, time=time, method='nearest'
         )
-        # SkewTだけ add_subplotで生成 (projection不要)
-        ax_emagram = fig.add_subplot(nrows, ncols, 1 + col)
         plot_emagram_msm(
-            ax_emagram,
+            fig,
+            col,
             dsi_point,
             AKITA_PIN_LAT,
             AKITA_PIN_LON,
-            "秋田"
+            "秋田",
+            nrows=nrows,
+            ncols=ncols
         )
+
         # 2〜6行目：秋田周辺の水平断面
         dsi = ds.sel(
             latitude=slice(AKITA_LAT_RANGE[0], AKITA_LAT_RANGE[1]),
