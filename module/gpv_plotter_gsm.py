92b4095017017de05bebdf8ee31cdcf4de88d7f7
queenworks <116941182+queenworks@users.noreply.github.com>
2025-06-12 15:15:36 +0900

    Update gpv_plotter_gsm.py

--- module/gpv_plotter_gsm.py
+++ module/gpv_plotter_gsm.py
@@ -12,7 +12,7 @@ from .plot_850hpa_temp_wind_700hpa_w import plot_850hpa_temp_wind_700hpa_w_gsm
 from .plot_850hpa_thetae_stream import plot_850hpa_thetae_stream_gsm
 from .plot_925hpa_temp_wind_dindex import plot_925hpa_temp_wind_dindex_gsm
 from .plot_surface_pressure_wind_precip import plot_surface_pressure_and_wind_gsm
-from .plot_emagram import plot_emagram_gsm
+from module.plot_emagram import plot_emagram_gsm_panel
 
 # GSM用関数は必ず「_gsm」付きでimport＆定義してください
 # 必要なものは随時ここに追加！
