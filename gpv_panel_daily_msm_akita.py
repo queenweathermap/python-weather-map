fa205cb2bc96749fcc1290e32bef224517db6099
queenworks <116941182+queenworks@users.noreply.github.com>
2025-06-13 20:58:57 +0900

    Update gpv_panel_daily_msm_akita.py

--- gpv_panel_daily_msm_akita.py
+++ gpv_panel_daily_msm_akita.py
@@ -8,10 +8,7 @@ import traceback
 import pandas as pd
 import xarray as xr
 
-from gpv_downloader import (
-    find_nearest_init, download_gpv_panel,
-    grib2_to_nc, MSM_PATTERNS, GPV_MIRROR_URLS
-)
+from gpv_downloader import find_existing_init_dt, download_gpv_panel, MSM_PATTERNS, GPV_MIRROR_URLS
 from module.panel_utils import make_nodata_weather_panel, make_local_weather_panel, align_datasets_common
 
 BASE_DIR = "./data"
