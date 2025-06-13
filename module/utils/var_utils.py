3df277feb2168305caa18a615851e3ee28b93c05
queenworks <116941182+queenworks@users.noreply.github.com>
2025-06-13 13:30:04 +0900

    Update var_utils.py

--- module/utils/var_utils.py
+++ module/utils/var_utils.py
@@ -5,5 +5,8 @@
 import numpy as np
 
 def get_var(ds, var):
-    """ds[var]が存在すればnp.asarrayで返す（なければNone）"""
-    return np.asarray(ds[var]) if var in ds else None
+    if var in ds:
+        x = ds[var]
+        # DataArrayやnumpy配列両対応
+        return np.asarray(x) if hasattr(x, "__array__") else x
+    return None
