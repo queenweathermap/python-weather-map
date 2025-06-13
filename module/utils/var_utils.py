d2740b78720db4c6d783d734bfdf46e3527b5365
queenworks <116941182+queenworks@users.noreply.github.com>
2025-06-13 14:42:59 +0900

    Update var_utils.py

--- module/utils/var_utils.py
+++ module/utils/var_utils.py
@@ -8,14 +8,13 @@ import xarray as xr
 
 def get_var(ds, var):
     """
-    dsから指定変数をNumPy配列で安全に取得
+    dsから指定変数をNumPy配列で安全に取得（xarrayバグ対応）
     """
     x = ds.get(var, None) if isinstance(ds, dict) else (ds[var] if var in ds else None)
     if x is None:
         return None
-    # ここで明示的に計算しきる
     if hasattr(x, "load"):
         x = x.load()
     if hasattr(x, "values"):
-        return np.array(x.values)   # .values属性でNumPy配列として受け取る
+        return np.array(x.values)
     return np.array(x)
