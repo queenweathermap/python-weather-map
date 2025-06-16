ea44fbf5adeb4e9944788ce4782adb3dece459a4
queenworks <116941182+queenworks@users.noreply.github.com>
2025-06-16 09:46:55 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,11 @@ import os
 SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
 SLACK_CHANNEL_ID = os.environ['SLACK_CHANNEL_ID']
 FILENAME = "test.png"
-FILESIZE = os.path.getsize(FILENAME)  # int型
+FILESIZE = os.path.getsize(FILENAME)
+
+print("ファイル名:", FILENAME)
+print("ファイルサイズ:", FILESIZE, type(FILESIZE))
+print("ファイル存在確認:", os.path.exists(FILENAME))
 
 headers = {
     "Authorization": f"Bearer {SLACK_BOT_TOKEN}",
@@ -20,4 +24,4 @@ res = requests.post(
     headers=headers,
     json=json_data
 )
-print(res.text)
+print("APIレスポンス:", res.text)
