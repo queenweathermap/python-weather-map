de950e3af706b57129861c51367e4e84822de3e7
queenworks <116941182+queenworks@users.noreply.github.com>
2025-06-16 10:04:44 +0900

    Update test.py

--- test.py
+++ test.py
@@ -5,27 +5,14 @@ SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
 SLACK_CHANNEL_ID = os.environ['SLACK_CHANNEL_ID']
 FILENAME = "test.png"
 
-# ファイル存在チェック
-assert os.path.exists(FILENAME), "ファイルがありません"
-
-print("ファイル名:", FILENAME)
-print("ファイルサイズ:", os.path.getsize(FILENAME), type(os.path.getsize(FILENAME)))
-print("ファイル存在確認:", os.path.exists(FILENAME))
-
 with open(FILENAME, "rb") as f:
-    res = requests.post(
+    response = requests.post(
         "https://slack.com/api/files.upload",
-        headers={
-            "Authorization": f"Bearer {SLACK_BOT_TOKEN}",
-        },
+        headers={"Authorization": f"Bearer {SLACK_BOT_TOKEN}"},
         data={
             "channels": SLACK_CHANNEL_ID,
-            "initial_comment": "Slack APIテスト送信 by Actions",
-            "title": "Slack upload test"
+            "initial_comment": "GitHub Actionsから画像テスト送信",
         },
-        files={
-            "file": (FILENAME, f, "image/png")
-        }
+        files={"file": f}
     )
-
-print(res.text)
+print(response.text)
