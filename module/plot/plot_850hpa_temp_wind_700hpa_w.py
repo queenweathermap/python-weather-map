1318592a81487e29514f191d5a5d988a48349d84
queenworks <116941182+queenworks@users.noreply.github.com>
2025-06-27 09:51:53 +0900

    Update plot_850hpa_temp_wind_700hpa_w.py

--- module/plot/plot_850hpa_temp_wind_700hpa_w.py
+++ module/plot/plot_850hpa_temp_wind_700hpa_w.py
@@ -9,8 +9,8 @@ import cartopy.feature as cfeature
 import matplotlib.pyplot as plt
 from module.utils.var_utils import get_var_2d, get_var
 
-plt.rcParams['font.family'] = 'sans-serif'
-plt.rcParams['font.sans-serif'] = ['DejaVu Sans']
+from module.plot.plot_utils import set_japanese_font, plot_no_data_japan_map
+set_japanese_font()  # 日本語フォントを全描画で有効化
 
 def get_lon_lat(ds):
     lon = get_var(ds, "longitude")
