aeae221807d49e2a4b874611825ad6e56253730d
queenworks <116941182+queenworks@users.noreply.github.com>
2025-06-28 14:51:36 +0900

    Update panel_definitions.py

--- module/panel_definitions.py
+++ module/panel_definitions.py
@@ -27,14 +27,16 @@ def get_panel_def_japan(ds_isobaric, ds_surf_instant):
     from module.plot.plot_surface_pressure_wind_precip import plot_surface_pressure_and_wind_msm
 
     return [
-        (plot_300hpa_height_wind, ds_isobaric, "300hPa高度・風"),
-        (plot_500hpa_vorticity, ds_isobaric, "500hPa渦度"),
-        (plot_700hpa_dindex_500hpa_temp, ds_isobaric, "700hPa湿数＋500hPa気温"),
-        (plot_850hpa_temp_wind_700hpa_w, ds_isobaric, "850hPa温度・風＋700hPa鉛直流"),
-        (plot_850hpa_thetae_stream, ds_isobaric, "850hPa θe流線"),
-        (plot_975hpa_temp_wind_dindex, ds_isobaric, "975hPa温度・風・湿数"),
-        (plot_925hpa_temp_wind_dindex, ds_isobaric, "925hPa温度・風・湿数"),
-        (plot_surface_pressure_and_wind_msm, ds_surf_instant, "地上気圧・風・降水"),
+        # GSM（上3段）
+        (plot_300hpa_height_wind, ds_gsm_isobaric, "300hPa高度・風"),
+        (plot_500hpa_vorticity, ds_gsm_isobaric, "500hPa渦度"),
+        (plot_700hpa_dindex_500hpa_temp, ds_gsm_isobaric, "700hPa湿数＋500hPa気温"),
+        # MSM（下5段）
+        (plot_850hpa_temp_wind_700hpa_w, ds_msm_isobaric, "850hPa温度・風＋700hPa鉛直流"),
+        (plot_850hpa_thetae_stream, ds_msm_isobaric, "850hPa θe流線"),
+        (plot_975hpa_temp_wind_dindex, ds_msm_isobaric, "975hPa温度・風・湿数"),
+        (plot_925hpa_temp_wind_dindex, ds_msm_isobaric, "925hPa温度・風・湿数"),
+        (plot_surface_pressure_and_wind_msm, ds_msm_surf_instant, "地上気圧・風・降水"),
     ]
 
 def get_panel_def_akita(ds_emagram, ds_850, ds_850_thetae, ds_925, ds_975, ds_surface):
@@ -50,14 +52,16 @@ def get_panel_def_akita(ds_emagram, ds_850, ds_850_thetae, ds_925, ds_975, ds_su
     from module.plot.plot_surface_pressure_wind_precip import plot_surface_pressure_and_wind_msm
 
     return [
+        # GSM（上1段）
         (plot_emagram, ds_emagram, "エマグラム"),
+        # MSM（下5段）
         (plot_850hpa_temp_wind_700hpa_w, ds_850, "850hPa気温・風・700hPa鉛直流"),
         (plot_850hpa_thetae_stream, ds_850_thetae, "850hPa相当温位・流線"),
         (plot_925hpa_temp_wind_dindex, ds_925, "925hPa気温・風・湿数"),
         (plot_975hpa_temp_wind_dindex, ds_975, "975hPa気温・風・湿数"),
         (plot_surface_pressure_and_wind_msm, ds_surface, "地上"),
         (None, None, ""),  # 7段目空欄
-        (None, None, ""),   # 8段目 空欄 ←NEW!
+        (None, None, ""),  # 8段目空欄
     ]
 
 # --- 拡張例: 任意ローカル（段数可変・テンプレート化しやすい） ---
