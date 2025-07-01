88fd5f2bd9e8d5c2967faa38e6d6ecebdcee5237
queenworks <116941182+queenworks@users.noreply.github.com>
2025-07-01 15:26:19 +0900

    Update gpv_panel_daily_japan.py

--- scripts/gpv_panel_daily_japan.py
+++ scripts/gpv_panel_daily_japan.py
@@ -12,7 +12,13 @@ import xarray as xr
 
 from module.utils.slack_utils import send_slack_text
 from module.core.gpv_downloader import list_files_on_server, GPV_MIRROR_URLS
-from module.plotter.gpv_plotter_universal import dump_grib_vars_auto, generate_universal_panel_and_notify, get_rh_fallback, get_apcp_3hr
+from module.plotter.gpv_plotter_universal import (
+    dump_grib_vars_auto,
+    generate_universal_panel_and_notify,
+    get_rh_fallback,
+    get_apcp_3hr,
+)
+
 
 def find_and_download_gpv_files(
     base_dir="./data",
