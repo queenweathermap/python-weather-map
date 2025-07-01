fa6f6db2016ce9799c7f85f41c20b05baaf2838b
queenworks <116941182+queenworks@users.noreply.github.com>
2025-07-01 12:28:02 +0900

    Update gpv_panel_daily_japan.py

--- scripts/gpv_panel_daily_japan.py
+++ scripts/gpv_panel_daily_japan.py
@@ -14,7 +14,7 @@ from module.panel_definitions import REGION_EXTENTS, get_panel_def_japan
 from module.utils.slack_utils import send_slack_text
 from module.utils.drive_utils import upload_to_drive
 from module.core.gpv_downloader import list_files_on_server, GPV_MIRROR_URLS
-from module.plotter.gpv_plotter_universal import dump_grib_vars
+from module.plotter.gpv_plotter_universal import dump_grib_vars_auto
 
 
 # ---- 変数・層ごと自動で最適なファイルを選ぶ関数
